667461e27b1f677995cc5dcff7df680f1614f0cf
Евгений <@HOME-PC>
2021-10-07 19:25:24 +0300

    Загрузка на сервер Heroku

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,9 @@ namespace TeConService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string con = "Server=(localdb)\\mssqllocaldb;Database=testsdb1;Trusted_Connection=True;";
-            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(con));
+            services.AddDbContext<ApplicationContext>(options =>
+            options.UseSqlite(
+                Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
         }
 
